328cd87a6067ff45deeedb5bfbe6eb6d77b1b261
Leone <leone.almeida@ba.estudante.senai.br>
2024-01-06 20:43:14 -0300

    primeiro commit

---
+++ js/racial.js
@@ -0,0 +1,28 @@
+function mensagemPeleClara() {
+    alert('kkkkkkkk');
+    window.location.href = 'https://youtu.be/Zu39r_L0_Pk?si=1uZ4Y38O-Z271Bst&t=24';
+  }
+  
+  function mensagemPeleMediaClaro() {
+    alert('Inocente...');
+    window.location.href = 'https://youtu.be/QxTvGtJ5H2c?si=5vTFC77Tq3jw8Lyi';
+  }
+  
+  function mensagemPeleMedia() {
+    window.location.href = 'https://youtu.be/ZoAFngyIuZY?si=kJJHuAKhF3g1-hRx';
+  }
+  
+  function mensagemPeleMediaEscuro() {
+    alert("Da pra arriscar...");
+  }
+  
+  function mensagemPeleEscuro() {
+    alert("Vai que é tua filhão");
+    window.location.href = 'https://youtu.be/DCz3SpY60bs?si=zQzGteveMUD8ZtpS';
+  }
+  
+  function mensagemPeleMuitoEscuro() {
+    alert("Passou Passou, já é sua!");
+    window.location.href = 'https://youtu.be/In4qd24lVSc?si=xU5WXlcL_ZBQNLsL&t=57';
+  }
+  
---
+++ js/renda.js
@@ -0,0 +1,25 @@
+function calculoRendaPerCapita() {
+    
+    let qttFamilia = parseInt(document.getElementById('qttFamilia').value) || 0;
+    let rendaPai = parseFloat(document.getElementById('rendaPai').value) || 0;
+    let rendaMae = parseFloat(document.getElementById('rendaMae').value) || 0;
+    let rendaSua = parseFloat(document.getElementById('rendaSua').value) || 0;
+    let rendaOutro = parseFloat(document.getElementById('rendaOutro').value) || 0;
+
+    rendaFamilia = rendaPai + rendaMae + rendaSua + rendaOutro;
+
+    rendaPerCapita = rendaFamilia / qttFamilia;
+
+    if (rendaPerCapita >= 1412) {
+        let mensagem = "Renda por capita: " +  rendaPerCapita.toFixed(1) + " Você não tem direito a cota";
+        alert(mensagem);
+        
+    } else {
+        let mensagem = "Renda por capita: " +  rendaPerCapita.toFixed(1) + " Você tem direito a cota";
+        alert(mensagem);
+ 
+    }
+
+    return false
+}
+
